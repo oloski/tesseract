2b9e01953939b08a25333a5ebbaab47d7aef64dc
romaintailhurat <romain.tailhurat@gmail.com>
2018-09-13 10:39:38 +0200

    [Queries] Deleting unused pop5 query function

--- queries.py
+++ queries.py
@@ -1,38 +1,6 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 # TODO cache ?
-def query_pop5():
-    URL = "http://graphdb.linked-open-statistics.org/repositories/pop5"
-
-    QUERY = """
-    PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX cog2017: <http://id.insee.fr/codes/cog2017/>
-    SELECT ?id ?code
-    WHERE {
-        ?id skos:notation ?code ;
-        rdf:type cog2017:DepartementOuCommuneOuArrondissementMunicipal .
-    } LIMIT 100
-    """
-
-    # The SPARQLWrapper will store network info and query logic
-    hackathon_endpoint = SPARQLWrapper(URL)
-    hackathon_endpoint.setQuery(QUERY)
-
-    # We want the resulting data in JSON format
-    hackathon_endpoint.setReturnFormat(JSON)
-    query_res = hackathon_endpoint.query().convert()
-
-    # Data structures for storing codes and URIs
-    uris = []
-    codes = []
-    for results in query_res["results"]["bindings"]:
-        code = results['code']['value']
-        codes.append(code)
-        uri = results['id']['value']
-        uris.append(uri)
-    
-    return codes
 
 def query_datasets():
     URL = "http://hackathon2018.ontotext.com/repositories/plosh"
@@ -97,7 +65,7 @@ def query_dimensions():
     
     return [{'label': result[keys[1]]['value'], 'value': result[keys[0]]['value']} for result in results]
     
-def query_mesures():
+def query_measures():
     
     sparql = SPARQLWrapper("http://hackathon2018.ontotext.com/repositories/plosh")
     sparql.setReturnFormat(JSON)
